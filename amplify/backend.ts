f5afe5a1ba925dd6dd3ceb11c7a1feaf5c500a7c
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2025-05-22 13:46:21 +0530

    1

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,96 @@
 import { defineBackend } from '@aws-amplify/backend';
+import { Effect, Policy, PolicyStatement } from "aws-cdk-lib/aws-iam";
+import { Bucket, HttpMethods } from "aws-cdk-lib/aws-s3";
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
 });
+
+const customBucketStack = backend.createStack("custom-bucket-stack");
+
+// Import existing bucket
+const customBucket = Bucket.fromBucketAttributes(customBucketStack, "MyCustomBucket", {
+  bucketArn: `arn:aws:s3:::baff-demo-storage-browser-test1`,
+  region: "us-east-1"
+});
+
+backend.addOutput({
+  storage: {
+    aws_region: customBucket.env.region,
+    bucket_name: customBucket.bucketName,
+    buckets: [
+      {
+        aws_region: customBucket.env.region,
+        bucket_name: customBucket.bucketName,
+        name: customBucket.bucketName,
+        paths: {
+          "public/*": {
+            guest: ["get", "list"],
+            authenticated: ["get", "list", "write", "delete"],
+          },
+        },
+      }
+    ]
+  },
+});
+
+const unauthPolicy = new Policy(backend.stack, "customBucketUnauthPolicy", {
+  statements: [
+    new PolicyStatement({
+      effect: Effect.ALLOW,
+      actions: ["s3:GetObject"],
+      resources: [`${customBucket.bucketArn}/public/*`],
+    }),
+    new PolicyStatement({
+      effect: Effect.ALLOW,
+      actions: ["s3:ListBucket"],
+      resources: [
+        `${customBucket.bucketArn}`,
+        `${customBucket.bucketArn}/*`
+      ],
+      conditions: {
+        StringLike: {
+          "s3:prefix": ["public/", "public/*"],
+        },
+      },
+    }),
+  ],
+});
+
+// Add the policies to the unauthenticated user role
+backend.auth.resources.unauthenticatedUserIamRole.attachInlinePolicy(
+  unauthPolicy,
+);
+
+const authPolicy = new Policy(backend.stack, "customBucketAuthPolicy", {
+  statements: [
+    new PolicyStatement({
+      effect: Effect.ALLOW,
+      actions: ["s3:GetObject", "s3:ListBucket"],
+      resources: [
+        `${customBucket.bucketArn}`,
+        `${customBucket.bucketArn}/*`
+      ],
+      conditions: {
+        StringLike: {
+          "s3:prefix": ["public/", "public/*"],
+        },
+      },
+    }),
+  ],
+});
+
+// Add the policies to the authenticated user role
+backend.auth.resources.authenticatedUserIamRole.attachInlinePolicy(
+  authPolicy,
+);
+
+// add cors policy to the bucket
+// customBucket.addCorsRule({
+//   allowedMethods: [HttpMethods.GET, HttpMethods.POST, HttpMethods.PUT, HttpMethods.DELETE],
+//   allowedOrigins: ["*"],
+//   allowedHeaders: ["*"],
+// });
